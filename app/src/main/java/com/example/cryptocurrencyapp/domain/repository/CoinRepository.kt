309ebcad87b137d0ca46b002f93360464d1eab5c
snehaawate <awate.sneha10@gmail.com>
2024-03-13 11:47:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cryptocurrencyapp/domain/repository/CoinRepository.kt
@@ -0,0 +1,11 @@
+package com.example.cryptocurrencyapp.domain.repository
+
+import com.example.cryptocurrencyapp.data.remote.dto.CoinDetailDto
+import com.example.cryptocurrencyapp.data.remote.dto.CoinDto
+
+interface CoinRepository {
+
+    suspend fun getCoins(): List<CoinDto>
+
+    suspend fun getCoinById(coinId: String): CoinDetailDto
+}
